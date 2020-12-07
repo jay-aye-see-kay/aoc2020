ccb5100f6cce4a2a8473d237cb459a87ed04f32e
Jack Rose <user@jackrose.co.nz>
2020-12-08 00:39:41 +1100

    day 5 part 1

---
+++ typescript/5/main.ts
@@ -0,0 +1,66 @@
+import * as fs from "fs";
+
+const FILENAME = "../input/5.input.txt";
+
+const parseInput = (filename: string) => {
+  return fs
+    .readFileSync(filename, "utf-8")
+    .split("\n")
+    .filter((l) => l.length);
+};
+
+type Pass = { row: number; col: number };
+type Range = { lower: number; upper: number };
+
+const parsePass = (pass: string): Pass => {
+  let row: number | Range = { lower: 0, upper: 127 };
+  pass
+    .slice(0, 7)
+    .split("")
+    .forEach((letter) => {
+      if (typeof row === "number") throw new Error("couldn't parse pass row");
+      row = binarySplit(row, letter === "F");
+    });
+
+  let col: number | Range = { lower: 0, upper: 7 };
+  pass
+    .slice(7, 10)
+    .split("")
+    .forEach((letter) => {
+      if (typeof col === "number") throw new Error("couldn't parse pass col");
+      col = binarySplit(col, letter === "L");
+    });
+
+  if (typeof row !== "number" || typeof col !== "number")
+    throw new Error("couldn't parse pass");
+
+  return { row, col };
+};
+
+// will blow up if not given a even number
+const binarySplit = ({ lower, upper }: Range, takeLower: boolean) => {
+  const range = upper + 1 - lower;
+  if (range == 2) return takeLower ? lower : upper;
+
+  let newRange;
+  if (takeLower) {
+    newRange = { lower, upper: upper - range / 2 };
+  } else {
+    newRange = { lower: lower + range / 2, upper };
+  }
+  return newRange;
+};
+
+const calcPassId = ({ row, col }: Pass): number => row * 8 + col;
+
+const bordingPasses = parseInput(FILENAME);
+
+let maxId = 0;
+bordingPasses.forEach((p) => {
+  const pass = parsePass(p);
+  const id = calcPassId(pass);
+  if (id > maxId) maxId = id;
+});
+
+// ans 996
+console.log("maxId", maxId);

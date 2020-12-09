6d4b8977a5e8ca0f2f4ecacd62d222aaedda949b
Jack Rose <user@jackrose.co.nz>
2020-12-09 17:22:36 +1100

    day 8 part 2

--- typescript/8/main.ts
+++ typescript/8/main.ts
@@ -5,6 +5,9 @@ const FILENAME = "../input/8.input.txt";
 const validOperations = ["nop", "acc", "jmp"] as const;
 type Operation = typeof validOperations[number];
 type Instruction = { op: Operation; arg: number };
+type Result =
+  | { kind: "success"; acc: number }
+  | { kind: "failure"; acc: number; message: string };
 
 const isOpValid = (op: any): op is Operation => validOperations.includes(op);
 
@@ -26,36 +29,72 @@ const parseInput = (filename: string): Instruction[] => {
     });
 };
 
+const runInstructions = (instructions: Instruction[]): Result => {
+  let ptr = 0;
+  const ptrHistory: number[] = [];
+  let acc = 0;
+
+  while (true) {
+    if ((ptr === instructions.length)) {
+      return { kind: "success", acc };
+    }
+
+    const instruction = instructions[ptr];
+
+    if (ptrHistory.includes(ptr)) {
+      const prevPtr = ptrHistory[ptrHistory.length - 1];
+      return {
+        kind: "failure",
+        acc,
+        message: `Loop detected at ptr: ${ptr}, (previous at ${prevPtr}). Accumulator value: ${acc}.`,
+      };
+    }
+    ptrHistory.push(ptr);
+
+    switch (instruction.op) {
+      case "nop":
+        ptr++;
+        break;
+      case "jmp":
+        ptr += instruction.arg;
+        break;
+      case "acc":
+        acc += instruction.arg;
+        ptr++;
+        break;
+      default:
+        assertNever(instruction.op);
+    }
+  }
+};
+
 const instructions = parseInput(FILENAME);
+const part1 = runInstructions(instructions);
+// ans 1087
+console.log("part 1: ", part1.acc);
 
-let ptr = 0;
-const ptrHistory: number[] = [];
-let acc = 0;
+// -- part 2 --
 
-while (true) {
-  const instruction = instructions[ptr];
-
-  if (ptrHistory.includes(ptr)) {
-    throw new Error(
-      `Loop detected at ptr: ${ptr}, (previous at ${
-        ptrHistory[ptrHistory.length - 1]
-      }). Accumulator value: ${acc}.`
-    );
-  }
-  ptrHistory.push(ptr);
-
-  switch (instruction.op) {
-    case "nop":
-      ptr++;
-      break;
-    case "jmp":
-      ptr += instruction.arg;
-      break;
-    case "acc":
-      acc += instruction.arg;
-      ptr++;
-      break;
-    default:
-      assertNever(instruction.op);
-  }
+const modInstruction = (i: number, instruction: Instruction): Instruction[] => {
+  const newInstructions = [...instructions];
+  newInstructions[i] = instruction
+  return newInstructions;
 }
+
+const alternativeInstructions: Instruction[][] = [];
+
+instructions.forEach((instruction, i) => {
+  if (instruction.op === "jmp") {
+    alternativeInstructions.push(modInstruction(i, {op: "nop", arg: instruction.arg }))
+  }else  if (instruction.op === "nop") {
+    alternativeInstructions.push(modInstruction(i, {op: "jmp", arg: instruction.arg }))
+  }
+})
+
+alternativeInstructions.forEach(instructions => {
+  const result = runInstructions(instructions);
+  if (result.kind === "success") {
+    // ans 780
+    console.log("part 2: ", result.acc)
+  }
+})

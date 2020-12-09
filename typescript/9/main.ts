416c9b0386ee63f84aa9572cc3add6ff88bdf163
Jack Rose <user@jackrose.co.nz>
2020-12-09 18:35:48 +1100

    day 9 part 2

--- typescript/9/main.ts
+++ typescript/9/main.ts
@@ -27,15 +27,43 @@ const isValid = (num: number, trailingNums: number[]) => {
   return false;
 };
 
+let magicNumber = -1;
+
 let ptr = PREAMBLE_LENGTH;
 while (ptr < nums.length) {
   const num = nums[ptr];
   const trailingNums = nums.slice(ptr - PREAMBLE_LENGTH, ptr);
 
   if (!isValid(num, trailingNums)) {
-    console.log("part 1: ", num);
-    break
+    magicNumber = num;
+    break;
   }
-
   ptr++;
+}
+console.log("part 1: ", magicNumber);
+
+// -- part 2 --
+
+const getValidSubset = (startingIndex: number) => {
+  let ptr = startingIndex;
+  let sum = nums[ptr];
+  ptr++;
+  while (sum <= magicNumber && ptr < nums.length) {
+    sum += nums[ptr];
+    if (sum === magicNumber) {
+      return nums.slice(startingIndex, ptr + 1);
+    }
+    ptr++;
+  }
+  return false;
+};
+
+for (let i = 0; i < nums.length; i++) {
+  const validSubset = getValidSubset(i);
+  if (validSubset) {
+    const minValid = Math.min(...validSubset);
+    const maxValid = Math.max(...validSubset);
+    console.log("part 2: ", minValid + maxValid);
+    break;
+  }
 }
